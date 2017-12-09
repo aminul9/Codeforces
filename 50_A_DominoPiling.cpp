de1a55fab0cbc2f9081538888a971abf1b60f196
aminul9 <aminultpi34@gmail.com>
2017-12-09 19:19:31 +0600

    Add files via upload

---
+++ 50_A_DominoPiling.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <fstream>
+#include <ctype.h>
+using namespace std;
+
+int main()
+{
+	//freopen("input.txt","r",stdin);
+	int M,N;
+	cin>>M>>N;
+
+	cout<<(M*N)/2;
+	return 0;
+}
