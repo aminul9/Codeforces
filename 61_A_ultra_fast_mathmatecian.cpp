bc037205dafd7839c0296bd1449c5291719cadb8
aminul9 <aminultpi34@gmail.com>
2017-12-13 15:07:50 +0600

    Add files via upload

---
+++ 61_A_ultra_fast_mathmatecian.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string s1,s2;
+    cin>>s1>>s2;
+    for(int i=0;i<s1.size();i++)
+    {
+        if(s1[i]==s2[i])
+            cout<<0;
+            else cout<<1;
+    }
+    return 0;
+
+}
